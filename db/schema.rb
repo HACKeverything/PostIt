ec2fd312cb17c091c4baeb4b93aa9ed912cb6855
shawn <management2e@gmail.com>
2015-12-15 19:08:29 +0800

    fix user to foreign key creator and week 1 part 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214062505) do
+ActiveRecord::Schema.define(version: 20151214112509) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20151214062505) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.integer  "post_id"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "posts", force: true do |t|
