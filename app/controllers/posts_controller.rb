c1e920d30a00808a9f522a6a8e2bcc8870a0cf28
shawn <management2e@gmail.com>
2015-12-17 16:39:58 +0800

    create post model-baced form(new, edit action) and with validations and strong parameters and before action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,47 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
+
   def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+
+    if @post.save
+      flash[:notice] = "You create a post"
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = "Update a post"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private 
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
+
+  def set_post
     @post = Post.find(params[:id])
   end
 end
