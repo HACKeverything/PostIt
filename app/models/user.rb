7d9d2f51a598ba564269f8ff56763ca6bd05c203
shawn <management2e@gmail.com>
2015-12-27 17:53:57 +0800

    User and Category slugs need update user slug column

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true 
   validates :password, presence: true, on: :create, length: { minimum: 5 }
+
+  before_save :generate_slug
+
+  def generate_slug
+    self.slug = self.uesrname.gsub(" ", "-").downcase
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151224030312) do
+ActiveRecord::Schema.define(version: 20151227084515) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
   end
 
   create_table "comments", force: true do |t|
@@ -41,6 +42,7 @@ ActiveRecord::Schema.define(version: 20151224030312) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "slug"
   end
 
   create_table "users", force: true do |t|
@@ -48,6 +50,7 @@ ActiveRecord::Schema.define(version: 20151224030312) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "slug"
   end
 
   create_table "votes", force: true do |t|
